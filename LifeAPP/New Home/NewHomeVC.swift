c705c2132f6473113c42ab62453b4c369f54eb14
顏淩育 <Louis@conquers.co>
2020-06-12 13:53:39 +0800

    add collection cell frame

--- LifeAPP/New Home/NewHomeVC.swift
+++ LifeAPP/New Home/NewHomeVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewHomeVC: UIViewController {
     
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewWillAppear(_ animated: Bool) {
         let image = UIImage()
         self.navigationController?.navigationBar.setBackgroundImage(image, for: .default)
@@ -20,8 +22,69 @@ class NewHomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let nib = UINib(nibName: "NewHomeCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "NewHomeCollectionViewCell")
         
     }
-    
+}
 
+extension NewHomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewHomeCollectionViewCell", for: indexPath) as! NewHomeCollectionViewCell
+        return cell
+    }
+    
+    
+}
+extension NewHomeVC: UICollectionViewDelegate {
+    
+}
+extension NewHomeVC: UICollectionViewDelegateFlowLayout {
+    /// 設定 Collection View 距離 Super View上、下、左、下間的距離
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 40, left: 25 * screenScaleWidth, bottom: 40, right: 25 * screenScaleWidth)
+    }
+    
+    ///  設定 CollectionViewCell 的寬、高
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - indexPath: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 364 * screenScaleWidth , height: 119)
+    }
+    
+    /// 滑動方向為「垂直」的話即「上下」的間距(預設為重直)
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(18)
+    }
+    
+    /// 滑動方向為「垂直」的話即「左右」的間距(預設為重直)
+    ///
+    /// - Parameters:
+    ///   - collectionView: _
+    ///   - collectionViewLayout: _
+    ///   - section: _
+    /// - Returns: _
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0 * screenScaleWidth
+    }
 }
