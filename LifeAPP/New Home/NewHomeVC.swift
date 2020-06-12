d0f67097df7e523d5559bcf5a8aad4b5a110f9a7
顏淩育 <Louis@conquers.co>
2020-06-12 08:41:19 +0800

    Add NewHomeVC

---
+++ LifeAPP/New Home/NewHomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewHomeVC.swift
+//  LifeAPP
+//
+//  Created by 顏淩育 on 2020/6/12.
+//  Copyright © 2020 顏淩育. All rights reserved.
+//
+
+import UIKit
+
+class NewHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
